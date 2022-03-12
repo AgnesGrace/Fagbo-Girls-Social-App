b1b6e400e65a79efc5bba7fcb05e3e18a62b19b2
AgnesGrace <98673311+AgnesGrace@users.noreply.github.com>
2022-03-12 17:30:13 +0100

    Updates

---
+++ precache-manifest.33faa8956a995b72713f2996146feeaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03e5f12ddc67c55d884b",
+    "url": "/Fagbo-Girls-Social-App/static/css/main.c4b78679.chunk.css"
+  },
+  {
+    "revision": "03e5f12ddc67c55d884b",
+    "url": "/Fagbo-Girls-Social-App/static/js/main.03e5f12d.chunk.js"
+  },
+  {
+    "revision": "16d4dd1c73fcfed4f37d",
+    "url": "/Fagbo-Girls-Social-App/static/js/1.16d4dd1c.chunk.js"
+  },
+  {
+    "revision": "63840bc1d4034e9fd9d1",
+    "url": "/Fagbo-Girls-Social-App/static/js/runtime~main.63840bc1.js"
+  },
+  {
+    "revision": "38d888ce677315448ec9354e68a68ec0",
+    "url": "/Fagbo-Girls-Social-App/index.html"
+  }
+];
